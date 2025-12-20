506b21b5ef7d999dc143b8992edd24c42b40c74c
marieme <soda-marieme.beye@ism.edu.sn>
2025-12-20 22:00:19 +0000

    Ready for deployment

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuration du port pour Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // MVC
 builder.Services.AddControllersWithViews();
 
